74a234f8b6004a84ad2fa257ee9c1b8c73ff3bf3
Vishal Nayal <vishalnayal325@email.com>
2026-05-17 02:16:57 +0530

    Fix photo upload size and preview error

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.get("/", (req, res) => {
     res.send("Student Management System API Running...");
